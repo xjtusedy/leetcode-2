2cec6269a89ff85dc63dbc3c94e25721eb23efa4
dingy007 <dingyi6698@qq.com>
2020-09-09 17:25:19 +0800

    test git

--- Mian1.java
+++ Mian1.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Mian1 {
+
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int len = sc.nextInt();
